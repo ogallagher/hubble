3340d4a671da700dad62a016e1f3c689188cd2be
Owen Gallagher <ogallagher2016@erhsnyc.net>
2016-11-07 11:55:02 -0500

    Updated server.js to actually serve

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
-var server = app.listen(8080);    //settings for testing on home computer
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
+var server = app.listen(port,ip);    //settings for testing on home computer
 
 app.use(express.static('public'));
