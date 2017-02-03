e209d373f529413a026d3858811dc8d0a0e62e5c
Owen Gallagher <ogallagher2016@erhsnyc.net>
2017-02-02 22:53:06 -0500

    Adding the information component
    > created games.json at the server side
    > server and client now speak with express and jquery respectively
    > the bubble tree is now more dynamic
    > “featured” is enabled, returning games with “featured” = “y”

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+
+var games = require("./games.json");
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
-var server = app.listen(port,ip);    //settings for testing on home computer
+app.use(express.static("public"));
 
-app.use(express.static('public'));
+app.get("/search", function(request,response) {
+        //search games.json by name, authors, and tags
+        });
+
+app.get("/category", function(request,response) {
+        //search games.json by categories key
+        });
+
+app.get("/featured", function(request,response) {
+        //search games by game.featured
+        var result = [];
+        
+        for (var i=0; i<games.length; i++) {
+            if (games[i].featured === "y") {
+                result.push(games[i]);
+            }
+        }
+        
+        response.send(JSON.stringify(result));
+        });
+
+var server = app.listen(port,ip);
