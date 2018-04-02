eb8cb1b65664b2d43a6e3f1e4c5b7acc6a9fa23b
Owen Gallagher <owengall@icloud.com>
2018-04-02 10:33:24 -0400

    Fixing server /random handler
    > the logic was flawed for generating a random subset of games from
    games[]
    > also fixing client’s display by calling home.updateAnchor() on window
    resize

--- server.js
+++ server.js
@@ -449,7 +449,7 @@ app.get("/random", function(request,response) {
         
         function nextRandom(previous) {
             if (previous.length < RESULT_MAX*0.5 && previous.length < games.byRating.length*0.75) {
-                var index = -1;
+                var index = Math.round(Math.random() * (games.byName.length-1));
         
                 while (previous.indexOf(index) != -1) {
                     index = Math.round(Math.random() * (games.byName.length-1));
@@ -478,7 +478,7 @@ app.get("/random", function(request,response) {
             }
         }
         
-        nextRandom([-1]);
+        nextRandom([]);
         });
 
 //REGISTER HANDLER
